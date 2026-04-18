1a942a31a7959c60cf6a9ba749c48ed6734e4a3f
May Tilokruangchai <s.tilokruangchai001@umb.edu>
2026-04-17 21:32:13 -0400

    Add loading scripts for various raw datasets

--- sql/staging/load_raw_tables.sql
+++ sql/staging/load_raw_tables.sql
@@ -16,4 +16,57 @@ FROM FILES (
   skip_leading_rows = 1
 );
 
--- (add the rest here)
+-- Order items
+LOAD DATA INTO `olist_analytics.raw_order_items`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_order_items_dataset.csv'],
+  skip_leading_rows = 1
+);
+
+-- Order payment
+LOAD DATA INTO `olist_analytics.raw_order_payments`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_order_payments_dataset.csv'],
+  skip_leading_rows = 1
+);
+
+-- Order reviews
+LOAD DATA INTO `olist_analytics.raw_order_reviews`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_order_reviews_dataset.csv'],
+  skip_leading_rows = 1
+);
+
+-- Products
+LOAD DATA INTO `olist_analytics.raw_products`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_products_dataset.csv'],
+  skip_leading_rows = 1
+);
+
+-- Sellers
+LOAD DATA INTO `olist_analytics.raw_sellers`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_sellers_dataset.csv'],
+  skip_leading_rows = 1
+);
+
+-- Geolocation
+LOAD DATA INTO `olist_analytics.raw_geolocation`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/olist_geolocation_dataset.csv'],
+  skip_leading_rows = 1
+);
+-- Product category translation
+LOAD DATA INTO `olist_analytics.raw_product_category_translation`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://olist-raw-data-stilok19/raw/product_category_name_translation.csv'],
+  skip_leading_rows = 1
+);
